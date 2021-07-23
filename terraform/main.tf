26d29ceb0a709e45f5dc9b1eb404e4b37fb622ed
Jared Holgate <jaredfholgate@gmail.com>
2021-07-23 15:06:22 +0100

    Add tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,13 @@ provider "azurerm" {
 
 module "stack_azure_hashicorp_vault" {
     source  = "app.terraform.io/jared-holgate-hashicorp/stack_azure_hashicorp_vault/jaredholgate"
-    environment = var.deployment_environment
     resource_group_name = format("%s%s", var.resource_group_name_prefix, var.deployment_environment)
     client_secret_for_unseal = var.client_secret_for_unseal
+    tags = {
+        environment = var.deployment_environment
+        version = var.deployment_version
+        date-deployed = var.deployment_date
+        application-name = "Vault demonstration"
+        owner = "Jared Holgate"
+    }
 }
