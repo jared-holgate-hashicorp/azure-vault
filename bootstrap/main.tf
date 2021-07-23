c650d12dd0023a7ff0fedf5d36a9c5eee209386a
Jared Holgate <jaredfholgate@gmail.com>
2021-07-23 17:49:44 +0100

    Tidy up

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,32 +1,32 @@
 variable "client_secret_for_role" {
-    type = string
+  type = string
 }
 
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-        }
-        tfe = {
-        }
-        github = {
-            source = "integrations/github"
-        }
-        azuread = {
-        }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
     }
-
-    backend "remote" {
-        organization = "jared-holgate-hashicorp"
-
-        workspaces {
-            name = "bootstrap"
-        }
+    tfe = {
     }
+    github = {
+      source = "integrations/github"
+    }
+    azuread = {
+    }
+  }
+
+  backend "remote" {
+    organization = "jared-holgate-hashicorp"
+
+    workspaces {
+      name = "bootstrap"
+    }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 provider "tfe" {
@@ -39,28 +39,28 @@ provider "azuread" {
 }
 
 locals {
-    environments = [ "test", "acpt", "prod" ]
-    organization = "jared-holgate-hashicorp"
+  environments = ["test", "acpt", "prod"]
+  organization = "jared-holgate-hashicorp"
 }
 
 data "azurerm_client_config" "current" {
 }
 
 resource "tfe_workspace" "jfh" {
-  for_each = { for env in local.environments : env => env }
+  for_each     = { for env in local.environments : env => env }
   name         = "azure-vault-${each.value}"
   organization = local.organization
   description  = "Demonstration HashiCorp Vault on Azure ${each.value}"
 }
 
 resource "tfe_team" "jfh" {
-  for_each = { for env in local.environments : env => env }
+  for_each     = { for env in local.environments : env => env }
   name         = "azure-vault-${each.value}"
   organization = local.organization
 }
 
 resource "tfe_team_access" "jfh" {
-  for_each = { for env in local.environments : env => env }
+  for_each     = { for env in local.environments : env => env }
   access       = "write"
   team_id      = tfe_team.jfh[each.value].id
   workspace_id = tfe_workspace.jfh[each.value].id
@@ -68,67 +68,67 @@ resource "tfe_team_access" "jfh" {
 
 resource "tfe_team_token" "jfh" {
   for_each = { for env in local.environments : env => env }
-  team_id = tfe_team.jfh[each.value].id
+  team_id  = tfe_team.jfh[each.value].id
 }
 
 resource "tfe_variable" "client_secret" {
-  for_each = { for env in local.environments : env => env }  
+  for_each     = { for env in local.environments : env => env }
   key          = "ARM_CLIENT_SECRET"
   value        = azuread_service_principal_password.jfh[each.value].value
   category     = "env"
   workspace_id = tfe_workspace.jfh[each.value].id
   description  = "The Azure Service Principal Client Secret"
-  sensitive = true
+  sensitive    = true
 }
 
 resource "tfe_variable" "client_id" {
-  for_each = { for env in local.environments : env => env }  
+  for_each     = { for env in local.environments : env => env }
   key          = "ARM_CLIENT_ID"
   value        = azuread_application.jfh[each.value].application_id
   category     = "env"
   workspace_id = tfe_workspace.jfh[each.value].id
   description  = "The Azure Service Principal Client Id"
-  sensitive = true
+  sensitive    = true
 }
 
 resource "tfe_variable" "tenant_id" {
-  for_each = { for env in local.environments : env => env }  
+  for_each     = { for env in local.environments : env => env }
   key          = "ARM_TENTANT_ID"
   value        = data.azurerm_client_config.current.tenant_id
   category     = "env"
   workspace_id = tfe_workspace.jfh[each.value].id
   description  = "The Azure Tenant Id"
-  sensitive = true
+  sensitive    = true
 }
 
 resource "tfe_variable" "subscription_id" {
-  for_each = { for env in local.environments : env => env }  
+  for_each     = { for env in local.environments : env => env }
   key          = "ARM_SUBSCRIPTION_ID"
   value        = data.azurerm_client_config.current.subscription_id
   category     = "env"
   workspace_id = tfe_workspace.jfh[each.value].id
   description  = "The Azure Subcription Id"
-  sensitive = true
+  sensitive    = true
 }
 
 resource "tfe_variable" "client_secret_for_unseal" {
-  for_each = { for env in local.environments : env => env }  
+  for_each     = { for env in local.environments : env => env }
   key          = "TF_VAR_client_secret_for_unseal"
   value        = azuread_service_principal_password.jfh.value
   category     = "env"
   workspace_id = tfe_workspace.jfh[each.value].id
   description  = "The Azure Client Secret required for unsealing Vault"
-  sensitive = true
+  sensitive    = true
 }
 
 resource "tfe_variable" "skip_provider_registration" {
-  for_each = { for env in local.environments : env => env }  
+  for_each     = { for env in local.environments : env => env }
   key          = "ARM_SKIP_PROVIDER_REGISTRATION"
   value        = "true"
   category     = "env"
   workspace_id = tfe_workspace.jfh[each.value].id
   description  = "Tell the Azure provider to skip provider registration on the subscription"
-  sensitive = false
+  sensitive    = false
 }
 
 data "github_user" "current" {
@@ -136,24 +136,24 @@ data "github_user" "current" {
 }
 
 resource "github_repository_environment" "repo_environment" {
-  for_each = { for env in local.environments : env => env }  
-  repository       = "azure-vault"
-  environment      = each.value
+  for_each    = { for env in local.environments : env => env }
+  repository  = "azure-vault"
+  environment = each.value
 
   dynamic "reviewers" {
-      for_each = each.value == "test" ? {} : { reviewer = "jaredfholgate" } 
-      content {
-          users = [ data.github_user.current.id ]
-      }
+    for_each = each.value == "test" ? {} : { reviewer = "jaredfholgate" }
+    content {
+      users = [data.github_user.current.id]
+    }
   }
 }
 
 resource "github_actions_environment_secret" "test_secret" {
-  for_each = { for env in local.environments : env => env } 
-  repository       = "azure-vault"
-  environment      = github_repository_environment.repo_environment[each.value].environment
-  secret_name      = "TF_API_TOKEN"
-  plaintext_value  = tfe_team_token.jfh[each.value].token
+  for_each        = { for env in local.environments : env => env }
+  repository      = "azure-vault"
+  environment     = github_repository_environment.repo_environment[each.value].environment
+  secret_name     = "TF_API_TOKEN"
+  plaintext_value = tfe_team_token.jfh[each.value].token
 }
 
 resource "azurerm_resource_group" "jfh" {
@@ -163,16 +163,17 @@ resource "azurerm_resource_group" "jfh" {
 }
 
 resource "azuread_application" "jfh" {
-  for_each = { for env in local.environments : env => env }
-  display_name               = "sp-azure-vault-${each.value}"
+  for_each     = { for env in local.environments : env => env }
+  display_name = "sp-azure-vault-${each.value}"
 }
 
 resource "azuread_service_principal" "jfh" {
-  for_each = { for env in local.environments : env => env }
-  application_id               = azuread_application.jfh[each.value].application_id
+  for_each       = { for env in local.environments : env => env }
+  application_id = azuread_application.jfh[each.value].application_id
 }
 
 resource "azuread_service_principal_password" "jfh" {
+  for_each             = { for env in local.environments : env => env }
   service_principal_id = azuread_service_principal.jfh[each.value].object_id
 }
 
@@ -180,18 +181,18 @@ data "azurerm_subscription" "current" {
 }
 
 resource "azurerm_role_assignment" "jfh" {
-  for_each = { for env in local.environments : env => env }
+  for_each             = { for env in local.environments : env => env }
   scope                = data.azurerm_subscription.current.id
   role_definition_name = "Owner"
   principal_id         = azuread_service_principal.jfh[each.value].object_id
 }
 
 module "resource_azure_ad_role_assignment" {
-  source              = "app.terraform.io/jared-holgate-hashicorp/resource_azure_ad_role_assignment/jaredholgate"
-  for_each = { for env in local.environments : env => env }
-  client_id           = data.azurerm_client_config.current.client_id
-  client_secret       = var.client_secret_for_role
-  principal_id        = azuread_service_principal.jfh[each.value].object_id
-  role_definition_id  = "e8611ab8-c189-46e8-94e1-60213ab1f814"
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  source             = "app.terraform.io/jared-holgate-hashicorp/resource_azure_ad_role_assignment/jaredholgate"
+  for_each           = { for env in local.environments : env => env }
+  client_id          = data.azurerm_client_config.current.client_id
+  client_secret      = var.client_secret_for_role
+  principal_id       = azuread_service_principal.jfh[each.value].object_id
+  role_definition_id = "e8611ab8-c189-46e8-94e1-60213ab1f814"
+  tenant_id          = data.azurerm_client_config.current.tenant_id
 }
