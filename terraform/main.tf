812a3a5cfb05cafdb909dff9cbc9df099096e307
Jared Holgate <jaredfholgate@gmail.com>
2021-07-28 16:11:47 +0100

    Pass an image version

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,11 @@ provider "azurerm" {
 }
 
 module "stack_azure_hashicorp_vault" {
-  source                   = "app.terraform.io/jared-holgate-hashicorp/stack_azure_hashicorp_vault/jaredholgate"
-  resource_group_name      = format("%s%s", var.resource_group_name_prefix, var.deployment_environment)
-  client_secret_for_unseal = var.client_secret_for_unseal
+  source                       = "app.terraform.io/jared-holgate-hashicorp/stack_azure_hashicorp_vault/jaredholgate"
+  resource_group_name          = format("%s%s", var.resource_group_name_prefix, var.deployment_environment)
+  client_secret_for_unseal     = var.client_secret_for_unseal
+  consul_cluster_image_version = "1.0.36"
+  vault_cluster_image_version  = "1.0.36"
   tags = {
     environment      = var.deployment_environment
     application-name = "Vault demonstration"
