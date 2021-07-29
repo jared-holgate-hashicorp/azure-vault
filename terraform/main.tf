bdbb5f0f09d14bf7017d6bd8e033b26a3cb88fa6
Jared Holgate <jaredfholgate@gmail.com>
2021-07-29 10:24:12 +0100

    Update VM version

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,8 +22,8 @@ module "stack_azure_hashicorp_vault" {
   source                       = "app.terraform.io/jared-holgate-hashicorp/stack_azure_hashicorp_vault/jaredholgate"
   resource_group_name          = format("%s%s", var.resource_group_name_prefix, var.deployment_environment)
   client_secret_for_unseal     = var.client_secret_for_unseal
-  consul_cluster_image_version = "1.0.36"
-  vault_cluster_image_version  = "1.0.36"
+  consul_cluster_image_version = "1.0.37"
+  vault_cluster_image_version  = "1.0.37"
   consul_cluster_size          = 3 
   vault_cluster_size           = 3
   tags = {
